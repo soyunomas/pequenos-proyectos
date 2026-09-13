6b10f7dcf383b3ab41832752e9825570577931f0
soyunomas <clementefeo@gmail.com>
2026-09-13 16:53:31 +0100

    feat(fluxfiles): bump app to 0.5.1

--- FluxFiles-Compose/app/build.gradle.kts
+++ FluxFiles-Compose/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soyunomas.fluxfiles"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.5.1"
         vectorDrawables { useSupportLibrary = true }
     }
 
