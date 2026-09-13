2b4f806e2d47ef80a6fd9fc90a477d49548b01fe
soyunomas <clementefeo@gmail.com>
2026-09-13 13:33:41 +0100

    fix(fluxfiles): opt in to Compose foundation APIs

--- FluxFiles-Compose/app/build.gradle.kts
+++ FluxFiles-Compose/app/build.gradle.kts
@@ -28,7 +28,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi"
+    }
     buildFeatures {
         compose = true
         buildConfig = true
